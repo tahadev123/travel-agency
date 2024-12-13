591bdcac6497970818f4671c2df5c0f9b6a56874
Taha Dehghan <taha.developer864@gmail.com>
2024-12-13 20:42:04 +0330

    debugging

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
 import HomePage from "@/components/templates/HomePage";
 import CheckInternet from "../core/utils/CheckInternet";
+import api from "@/core/config/api";
 
 async function getTour() {
-  const res = await fetch("http://localhost:6500/tour");
+  const res = await api.get("/tour");
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
   return res.json();
 }
 
